2efc23d2f3d8adcdfb0997c3cd07c81692135f21
batchgott <shumpelstetter@gmail.com>
2022-03-08 14:58:57 +0100

    Create generic repository

---
+++ backend/Services/ARepository.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace simple_chatrooms_backend.Services {
+    public abstract class ARepository<T, Id> : IRepository<T, Id> where T : class {
+
+        protected readonly SimpleChatroomsContext _context;
+        public ARepository(SimpleChatroomsContext context) {
+            _context = context;
+        }
+
+        public void Add(T entity) {
+            _context.Set<T>().Add(entity);
+        }
+
+        public void AddRange(IEnumerable<T> entities) {
+            _context.Set<T>().AddRange(entities);
+        }
+
+        public IEnumerable<T> GetAll() {
+            return _context.Set<T>().ToList();
+        }
+
+        public T GetOne(Id id) {
+            return _context.Set<T>().Find(id);
+        }
+
+        public void Remove(T entity) {
+            _context.Set<T>().Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entities) {
+            _context.Set<T>().RemoveRange(entities);
+        }
+
+        public bool Save() {
+            return (_context.SaveChanges() >= 0);
+        }
+
+        public void Update(T entity) {
+            _context.Set<T>().Update(entity);
+        }
+    }
+}
---
+++ backend/Services/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace simple_chatrooms_backend.Services {
+    interface IRepository<T, Id> where T : class {
+        T GetOne(Id id);
+        IEnumerable<T> GetAll();
+        void Add(T entity);
+        void AddRange(IEnumerable<T> entities);
+        void Update(T entity);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+        bool Save();
+    }
+}
