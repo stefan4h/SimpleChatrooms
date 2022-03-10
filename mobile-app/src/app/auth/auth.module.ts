1dd30756ccf43231b5a993fc6f20e327cd0dbb6a
batchgott <shumpelstetter@gmail.com>
2022-03-10 19:31:35 +0100

    Create register form

--- mobile-app/src/app/auth/auth.module.ts
+++ mobile-app/src/app/auth/auth.module.ts
@@ -1,6 +1,6 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import {IonicModule} from '@ionic/angular';
 
@@ -16,7 +16,8 @@ import {RegisterComponent} from './components/register/register.component';
     CommonModule,
     FormsModule,
     IonicModule,
-    AuthPageRoutingModule
+    AuthPageRoutingModule,
+    ReactiveFormsModule,
   ],
   declarations: [
     AuthPage,
--- mobile-app/src/app/auth/components/register/register.component.ts
+++ mobile-app/src/app/auth/components/register/register.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AbstractControl, FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-register',
@@ -7,8 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  registerForm: FormGroup;
 
-  ngOnInit() {}
+  constructor(private formBuilder: FormBuilder) {
+  }
 
+  ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      firstname: ['', Validators.required],
+      lastname: ['', Validators.required],
+      username: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  get firstname(): AbstractControl {
+    return this.registerForm.get('firstname');
+  }
+
+  get lastname(): AbstractControl {
+    return this.registerForm.get('lastname');
+  }
+
+  get username(): AbstractControl {
+    return this.registerForm.get('username');
+  }
+
+  get password(): AbstractControl {
+    return this.registerForm.get('password');
+  }
 }
