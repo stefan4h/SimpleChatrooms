7decb955bccc4b4b58cd538c1674842dbc8f42aa
batchgott <shumpelstetter@gmail.com>
2022-03-13 10:53:56 +0100

    Create login form

--- mobile-app/src/app/auth/components/login/login.component.ts
+++ mobile-app/src/app/auth/components/login/login.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AbstractControl, FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {finalize} from "rxjs/operators";
+import {AuthService} from "../../../services/auth/auth.service";
+import {ToastService} from "../../../services/toast.service";
 
 @Component({
   selector: 'app-login',
@@ -7,8 +11,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+  loading: boolean = false;
 
-  ngOnInit() {}
+  constructor(private formBuilder: FormBuilder,
+              private authService: AuthService,
+              private toastService:ToastService) {
+  }
 
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', [Validators.required]]
+    });
+  }
+
+  register() {
+    this.loading = true;
+
+    this.authService.login(
+      this.username.value,
+      this.password.value
+    ).pipe(finalize(() => this.loading = false))
+      .subscribe(
+        () => this.toastService.success('Welcome back!'),
+        error => this.toastService.error('An error occurred while logging in')
+      );
+  }
+
+  get username(): AbstractControl {
+    return this.loginForm.get('username');
+  }
+
+  get password(): AbstractControl {
+    return this.loginForm.get('password');
+  }
 }
