07625acb9396dc709b490e1562ad765381629de0
batchgott <shumpelstetter@gmail.com>
2022-03-14 12:53:45 +0100

    Display username in settings

--- mobile-app/src/app/home/components/settings/settings.component.ts
+++ mobile-app/src/app/home/components/settings/settings.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {AuthService} from "../../../services/auth/auth.service";
+import {Observable} from "rxjs";
+import {User} from "../../../models/user.model";
 
 @Component({
   selector: 'app-settings',
@@ -8,10 +10,13 @@ import {AuthService} from "../../../services/auth/auth.service";
 })
 export class SettingsComponent implements OnInit {
 
+  user$: Observable<User>
+
   constructor(public authService: AuthService) {
   }
 
   ngOnInit() {
+    this.user$ = this.authService.user$;
   }
 
 }
